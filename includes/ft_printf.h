b183c703a8ffdba69372d961eddefe983f02154b
marcnava <marcnava@student.42madrid.com>
2025-09-21 00:15:06 +0200

    Fixed norme

--- includes/ft_printf.h
+++ includes/ft_printf.h
@@ -6,14 +6,13 @@
 /*   By: marcnava <marcnava@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 19:57:48 by marcnava          #+#    #+#             */
-/*   Updated: 2025/01/24 12:02:29 by marcnava         ###   ########.fr       */
+/*   Updated: 2025/09/21 00:14:50 by marcnava         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-/* API pública: solo el prototipo de ft_printf */
-int		ft_printf(char const *format, ...);
+int	ft_printf(char const *format, ...);
 
-# endif
+#endif
